1c56ad2e01d05112c2bae28fd127bbc4ede67858
juhwanHeo <juhwanheo.dev@gmail.com>
2024-09-19 15:55:52 +0900

    feat: Add kotlin project

---
+++ kafka-mvc-kotlin/src/test/kotlin/com/kafka/example/KafkaMvcKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kafka.example
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KafkaMvcKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
